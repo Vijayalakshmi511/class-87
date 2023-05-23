e890f3f49d805bfad30987c779e9a6e351e1ea46
Vijayalakshmi511 <73171086+Vijayalakshmi511@users.noreply.github.com>
2023-05-23 15:36:30 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCMWeu1MuZGowuinDoxzWeCmC3mBI-nImM",
+  authDomain: "storytelling-app-5ec82.firebaseapp.com",
+  databaseURL: "https://storytelling-app-5ec82-default-rtdb.firebaseio.com",
+  projectId: "storytelling-app-5ec82",
+  storageBucket: "storytelling-app-5ec82.appspot.com",
+  messagingSenderId: "954684490980",
+  appId: "1:954684490980:web:0eab6baa05ab21a40658f1"
+};
